d993a63ee2f149764c4da087db21c689862d3ffe
Santos Odigie <santosodigie104@gmail.com>
2024-11-17 22:12:04 +0000

    added button panels addie list to hold tasks added event listeners that add tasks and remove tasks

--- GUI/src/GUI.java
+++ GUI/src/GUI.java
@@ -1,15 +1,34 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI {
+public class GUI  implements ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+
     public GUI() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER); // adds the panel to the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // what happens when they close the frame
@@ -19,5 +38,11 @@ public class GUI {
     }
     public static void main(String[] args) {
         new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks " + count);
     }
 }
